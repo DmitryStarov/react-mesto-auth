6a908c45b9a9035040f18cf114c8a2b268e5da63
DmitryStarov <314263@bk.ru>
2023-06-12 02:06:16 +0800

    featurse/ProtctedRoute

--- src/components/ProtectedRoute.js
+++ src/components/ProtectedRoute.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Navigate } from "react-router-dom";
 
-export default function ProtctedRoute({ element: Component, isLogged }) {
+export default function ProtctedRoute({ element: Component, isLoggedIn }) {
   return isLoggedIn ? children : <Navigate to="/sign-in" replace />;
 }
