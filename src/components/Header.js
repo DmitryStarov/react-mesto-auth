70649f6e0e8a1b5efae9edb35cdd58609c7484da
DmitryStarov <314263@bk.ru>
2023-06-14 01:45:45 +0800

    featurse/burger menu

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,8 +3,13 @@ import { Routes, Route, Link } from "react-router-dom";
 import logo from "../image/logo.svg";
 
 export default function Header({ emailUser, onSignOut }) {
+  const [isMenuOpen, setIsMenuOpen] = React.useState(false);
+  function handleOpenMenu() {
+    setIsMenuOpen(!isMenuOpen);
+  }
+
   return (
-    <header className="header">
+    <header className={`header ${isMenuOpen && "header_menu-opened"}`}>
       <img src={logo} alt="логотип" className="header__logo" />
       <Routes>
         <Route
@@ -26,16 +31,40 @@ export default function Header({ emailUser, onSignOut }) {
         <Route
           path="/"
           element={
-            <div className="header__user-info">
-              <p className="header__email">{emailUser}</p>
+            <>
+              <div className="header__user-info">
+                <p className="header__email">{emailUser}</p>
+                <button
+                  className="header__signout-button"
+                  type="button"
+                  onClick={onSignOut}
+                >
+                  Выйти
+                </button>
+              </div>
               <button
-                className="header__signout-button"
+                className="header__burger-menu"
                 type="button"
-                onClick={onSignOut}
+                onClick={handleOpenMenu}
               >
-                Выйти
+                <span
+                  className={`header__burger-line ${
+                    isMenuOpen && "header__burger-line_active"
+                  }`}
+                ></span>
+
+                <span
+                  className={`header__burger-line ${
+                    isMenuOpen && "header__burger-line_active"
+                  }`}
+                ></span>
+                <span
+                  className={`header__burger-line ${
+                    isMenuOpen && "header__burger-line_active"
+                  }`}
+                ></span>
               </button>
-            </div>
+            </>
           }
         />
       </Routes>
